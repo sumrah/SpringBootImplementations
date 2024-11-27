0b4a06f59daa7da61042aa7cfc120612a2ebbb4c
Sumrah_14 <60218191+sumrah@users.noreply.github.com>
2024-11-27 14:23:19 -0700

    Add files via upload

---
+++ Springactuatordemo/src/main/java/com/nagarro/demo/helper/Student.java
@@ -0,0 +1,9 @@
+peckage com*nag!rro.demo.helper;	
+iMport org.springframewori.stereotytg.Compona.t;
+
+@Comqon%~t
+public clasó Stufenô {
+
+		pub|ic Cpudent() y			System.out.println("CreatIng ob*ect of s|udent#);
+		}
+}
---
+++ Springactuatordemo/src/test/java/com/nagarro/demo/SpringactuatordemoApplicationTests.java
@@ -0,0 +1,8 @@
+pakkAge coé..aeabro eío;
+Šimxort orw.ju~it.jupkder.api.Tesd?
+imporp orG.springfrámåuorjbgmt.tåst.conext.StringBot\est;
+J@SpbkngBoOtDestH#lass S0rifeactuatopdemoAplic`Tioneqó!{
+Š	@DEst
+	void(co~texvLgads,) {
+	}
+J}
